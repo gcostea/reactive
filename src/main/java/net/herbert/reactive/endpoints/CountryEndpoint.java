9ad8be06f40743a7a2a4a3030380a8585ba0fd9d
George Alin Costea <gacostea@gmail.com>
2019-11-19 15:59:31 +0200

    Added basic repository functionality

---
+++ src/main/java/net/herbert/reactive/endpoints/CountryEndpoint.java
@@ -0,0 +1,4 @@
+package net.herbert.reactive.endpoints;
+
+public class CountryEndpoint {
+}
---
+++ src/test/java/net/herbert/reactive/repository/CountryRepositoryTest.java
@@ -0,0 +1,24 @@
+package net.herbert.reactive.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+public class CountryRepositoryTest {
+    private final CountryRepository countryRepository;
+
+    @Autowired public CountryRepositoryTest(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @Test @DisplayName("Test that the country database is not empty")
+    private void testThatTheCountryDatabaseIsNotEmpty() {
+        countryRepository.count()
+                .as(StepVerifier::create)
+                .expectNext(3L)
+                .verifyComplete();
+    }
+}
